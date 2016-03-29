5691f40e4c36c35ae9aecb2a072904b29e73fd29
Darran Kelinske <Darran.Kelinske@servicemesh.com>
2016-03-29 15:39:04 -0500

    first commit

---
+++ app/src/main/java/net/tipit/okhttpspike/MainActivity.java
@@ -0,0 +1,71 @@
+package net.tipit.okhttpspike;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
+
+        new DownloadImageTask(mImageView)
+                .execute("https://pbs.twimg.com/profile_images/61655057/2425718692_1783fe0913_b.jpg");
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+
+            final OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(urls[0])
+                    .build();
+
+            Response response = null;
+            Bitmap mIcon11 = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (response.isSuccessful()) {
+                try {
+                    mIcon11 = BitmapFactory.decodeStream(response.body().byteStream());
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
+                }
+
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
